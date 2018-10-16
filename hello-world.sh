c5050410ea457b58046679783b1132459da40795
bonbisu <fernando.costa30@fatec.sp.gov.br>
2018-10-16 15:16:15 -0300

    about debug mode
    some code to debug and permit access to script

--- hello-world.sh
+++ hello-world.sh
@@ -1,4 +1,13 @@
-#!/bin/bash 
+#!/bin/bash -x
+
+# Shegang calls what interpreter will runs the script
+# Inserting -x on shebang will runs on debug mode,
+# we can also call '$ bash -x script.sh' to debug.
+# pound sign (#) can be used to comment code.
+
+## If we want to access some script anywhere, we need to permit this
+## by calling '$ chmod +x script.sh' and putting the file in 
+## some path included in $PATH variable.
 
 echo "Hello World"
 
