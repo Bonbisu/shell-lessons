f6a74ee72f1e71ce82a19221ef62f3b4a364045a
bonbisu <fernando.costa30@fatec.sp.gov.br>
2018-10-16 16:09:36 -0300

    killing other same script

--- greeting.sh
+++ greeting.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-echo -e "Enter your name: \c"
+echo "Hello $1"
 
-read INPUT_NAME
-
-echo "Hello $INPUT_NAME"
+# Using $n we cand take outside arguments to put in our script
+# when we hit 2 digit arg we write ${nn} e.g. '${15}' $0 is the
+# script name itself. Using $* will take all params except script
+# name. $# will count all params except script name. 
 
 exit 0
