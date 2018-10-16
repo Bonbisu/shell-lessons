5cbd6bd91af075b31063041faba74601cdcf69ca
bonbisu <fernando.costa30@fatec.sp.gov.br>
2018-10-16 16:20:48 -0300

    add a test condition
    test if have at least on param

--- greeting.sh
+++ greeting.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+if  (( $# < 1))
+  then
+    echo "Usage: $0 <name>"
+    exit 1
+  #statements
+fi
 echo "Hello $1"
 
 # Using $n we cand take outside arguments to put in our script
 # when we hit 2 digit arg we write ${nn} e.g. '${15}' $0 is the
 # script name itself. Using $* will take all params except script
-# name. $# will count all params except script name. 
+# name. $# will count all params except script name.
 
 exit 0
