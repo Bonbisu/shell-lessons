8217c9b874af14c4538b2a6e0bc0b2cc2ee87793
bonbisu <fernando.costa30@fatec.sp.gov.br>
2018-10-23 15:26:52 -0300

    funny countdown
    care bear song

---
+++ ursal.sh
@@ -0,0 +1,14 @@
+sleep 0.5
+echo -e "		Os \u266a"
+sleep 0.5
+echo -e "	ursinhos \u266b"
+sleep 0.5
+echo -e "	carinhosos \u266a"
+I=5
+sleep 1.5
+while [[ $I -ge 1 ]]
+do
+echo $I
+sleep 0.5
+(( I-- ))
+done
