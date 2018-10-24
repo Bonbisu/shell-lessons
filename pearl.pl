8078c175e9636811e0cff85579ae2b3936ab6019
bonbisu <fernando.costa30@fatec.sp.gov.br>
2018-10-24 15:22:33 -0300

    commenting perl code

--- pearl.pl
+++ pearl.pl
@@ -1,15 +1,17 @@
 #!/usr/bin/perl
 
 print "Enter a valid UK postcode... ";
-
+# try to catch the first arg
 $pcode = $ARGV[0]; # this take the first arg
+
+# test if already have a postcode
 if ( ! $pcode ) {
     $pcode = <STDIN>;
 } 
 
-chomp($pcode);
+chomp($pcode); # removes trailing symbols (like \n) at end of line
 
-$pcode = uc $pcode;
+$pcode = uc $pcode; # uc parse to Uper Case
 
 # REGEXP PATTERN:
 # ^ for first line
@@ -21,6 +23,7 @@ $pcode = uc $pcode;
 
 $pattern = '^[A-Z]{1,2}[0-9]{1,2}[A-Z]?\s[0-9][A-Z]{2}$';
 
+# comparing the pcode to pattern w/ regexp comparator (=~) and adding word boundaries (/) 
 if ($pcode =~ /$pattern/) {
     print "We can deliver to: $pcode \n";
 } else {
